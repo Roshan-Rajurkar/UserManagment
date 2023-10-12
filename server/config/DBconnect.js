e08c385bab55d1bb3a4a33f58c21bbea0cf454c5
Roshan-Rajurkar <roshangit50@gmail.com>
2023-10-12 06:22:57 +0530

    hosting changes

--- server/config/DBconnect.js
+++ server/config/DBconnect.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose')
-
-const mongoURL = 'mongodb+srv://Mind123:Mind123@cluster0.iay2uo3.mongodb.net/?retryWrites=true&w=majority/test';
+require("dotenv").config();
 
 const DBconnect = async () => {
-    const response = await mongoose.connect(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log('connected to', response.connection.host)
+    try {
+        const response = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(response.connection.host)
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 module.exports = DBconnect;
--- server/server.js
+++ server/server.js
@@ -3,17 +3,21 @@ const app = express();
 const cors = require('cors')
 const userRoutes = require('./routes/userRoutes')
 const DBconnect = require('./config/DBconnect')
+require("dotenv").config();
 
-app.use(cors());
-app.use(express.json())
+const PORT = process.env.PORT || 5000;
+
+// middleware
+const corsOptions = {
+    origin: "http://localhost:3000",
+}
+app.use(express.json());
+app.use(cors(corsOptions));
 
 DBconnect();
 
-app.get('/', (req, res) => {
-    res.send('working api')
-})
 app.use('/', userRoutes)
 
-app.listen(5000, () => {
-    console.log('running on port number 5000')
+app.listen(PORT, () => {
+    console.log(`running on port number ${PORT}`)
 })
