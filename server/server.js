6a21a7ef0ad27313642985268fa2fef2cf1f4dfc
Roshan-Rajurkar <roshangit50@gmail.com>
2023-10-12 16:49:30 +0530

    complete app

--- server/server.js
+++ server/server.js
@@ -12,18 +12,15 @@ const corsOptions = {
     origin: "http://localhost:3000",
 }
 
-if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 app.use(express.json());
 app.use(cors(corsOptions));
 
 DBconnect();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
 
 app.use('/', userRoutes)
 
