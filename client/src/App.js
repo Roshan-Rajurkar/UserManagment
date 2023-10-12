10c05e8653ab7693fce66020759632ebd037adcd
Roshan-Rajurkar <roshangit50@gmail.com>
2023-10-12 07:45:50 +0530

    mte

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
+import React, { useState, useEffect } from 'react'
 import Dashboard from "./components/dashboard/Dashboard";
 import Login from "./form/login/Login";
 import Register from "./form/register/Register";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("https://usermanagement-backend-17ta.onrender.com")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
   return (
     <div className="App">
+      <h1>{message}</h1>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
