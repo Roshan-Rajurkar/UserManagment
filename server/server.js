c8ed89fcce6c0803b8e91bbd6d0022807fc28c6a
Roshan-Rajurkar <roshangit50@gmail.com>
2023-10-13 23:28:13 +0530

    root

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // middleware
 const corsOptions = {
-    origin: "https://usermanagment-frontend-owwz.onrender.com/",
+    origin: "https://usermanagment-client.onrender.com",
 }
 
 
